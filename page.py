f76433ee3e028f7b6488b9d008c61e347a4df1f7
jlane9 <jlane@sportinginnovations.com>
2016-03-29 15:01:32 -0500

    Push to PyPI

--- page.py
+++ page.py
@@ -14,7 +14,7 @@ import re
 __author__ = 'jlane'
 __copyright__ = 'Copyright (c) 2016 FanThreeSixty'
 __license__ = "MIT"
-__version__ = '0.0.1'
+__version__ = '0.2'
 __contact__ = 'jlane@fanthreesixty.com'
 __status__ = 'Alpha'
 __docformat__ = 'reStructuredText'
