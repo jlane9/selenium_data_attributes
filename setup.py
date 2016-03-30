38596e7b4487b2aacf03d7ecbed9347e5316b659
jlane9 <jlane@Johns-MacBook-Air.local>
2016-03-30 09:53:17 -0500

    Update 0.2.4

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name='sda',
-    version='0.2.3',
+    version='0.2.4',
     packages=['sda'],
     description='A wrapper for Selenium. This library uses custom data attributes to accelerate testing '
     'through the Selenium framework',
     author='John Lane',
     author_email='jlane@fanthreesixty.com',
     url='https://github.com/jlane9/selenium-data-attributes',
-    download_url='https://github.com/jlane9/selenium-data-attributes/tarball/0.2.3',
+    download_url='https://github.com/jlane9/selenium-data-attributes/tarball/0.2.4',
     keywords='testing selenium qa web automation',
     install_requires=['selenium>=2.48.0'],
     license='MIT',
