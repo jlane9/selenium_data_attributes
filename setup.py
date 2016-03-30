48f20cf746eb2ba27f2373505e24fd52e1e6f0b8
jlane9 <jlane@Johns-MacBook-Air.local>
2016-03-30 09:45:54 -0500

    Update release 0.2.3

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-# from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='selenium-data-attributes',
-      packages=find_packages(where="selenium_data_attributes"),
-      version='0.2.3',
-      description='A wrapper for Selenium. This library uses custom data attributes to accelerate testing '
-                  'through the Selenium framework',
-      author='John Lane',
-      author_email='jlane@fanthreesixty.com',
-      url='https://github.com/jlane9/selenium-data-attributes',
-      download_url='https://github.com/jlane9/selenium-data-attributes/tarball/0.2',
-      keywords='testing selenium qa web automation',
-      install_requires=['selenium'],
-      license='MIT',
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7'])
+
+setup(
+    name='sda',
+    version='0.2.3',
+    packages=['sda'],
+    description='A wrapper for Selenium. This library uses custom data attributes to accelerate testing '
+    'through the Selenium framework',
+    author='John Lane',
+    author_email='jlane@fanthreesixty.com',
+    url='https://github.com/jlane9/selenium-data-attributes',
+    download_url='https://github.com/jlane9/selenium-data-attributes/tarball/0.2.3',
+    keywords='testing selenium qa web automation',
+    install_requires=['selenium>=2.48.0'],
+    license='MIT',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7'])
