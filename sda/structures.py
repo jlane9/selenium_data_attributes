a9b1baa5c33c65327df8bc2fc521fb37d13321d4
jlane9 <jlane@fanthreesixty.com>
2016-05-12 20:08:11 -0500

    0.5.2 release

--- sda/structures.py
+++ sda/structures.py
@@ -28,7 +28,7 @@ from mixins import *
 __author__ = 'jlane'
 __copyright__ = 'Copyright (c) 2016 FanThreeSixty'
 __license__ = "MIT"
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 __contact__ = 'jlane@fanthreesixty.com'
 __status__ = 'Alpha'
 __docformat__ = 'reStructuredText'
