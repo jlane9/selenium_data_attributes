3be6827b6e0471e07173f5a9b7d11140b9a8fbe2
jlane9 <jlane@fanthreesixty.com>
2016-07-04 23:45:58 -0500

    0.7.0 release

--- sda/page.py
+++ sda/page.py
@@ -10,7 +10,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 __author__ = 'jlane'
 __copyright__ = 'Copyright (c) 2016 FanThreeSixty'
 __license__ = "MIT"
-__version__ = '0.6.9'
+__version__ = '0.7.0'
 __contact__ = 'jlane@fanthreesixty.com'
 __status__ = 'Alpha'
 __docformat__ = 'reStructuredText'
