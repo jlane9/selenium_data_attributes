335ce70414ffecbfc506ce2c22a5a7e4aeeca1a1
John Lane <jlane@sportinginnovations.com>
2018-04-09 09:50:13 -0500

    Streamline imports for Element, Page and Site

--- sda/__init__.py
+++ sda/__init__.py
@@ -5,16 +5,16 @@
 
 """
 
-from sda import element
 from sda import locators
 from sda import mixins
-from sda import page
-from sda import site
 from sda import structures
+from sda.element import Element
+from sda.page import Page
+from sda.site import Site
 
 __author__ = 'jlane'
 __email__ = 'jlane@fanthreesixty.com'
 __license__ = "MIT"
 __version__ = '0.9.1'
 
-__all__ = ['element', 'locators', 'page', 'shortcuts', 'site', 'structures']
+__all__ = ['locators', 'shortcuts', 'structures', 'Element', 'Page', 'Site']
